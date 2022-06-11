89c9160e9b5842079c74bb32e1dc4e8774e26180
Alchimis <79978300+Alchimis@users.noreply.github.com>
2022-06-11 09:50:18 +0300

    Поправил форматирование и опечатки, добавил исключения, обработку исключений в одном месте, сделал проверку переплнения

--- calc/Program.cs
+++ calc/Program.cs
@@ -4,33 +4,43 @@
     {
         public static int Main(string[] argvs)
         {
-            Console.WriteLine("Вводите математичекие выражения, которые хотите выполнить \n" +
-                "Если захотите выйдти из приложенияя прото введите ENTER");
+            Console.WriteLine("Вводите математические выражения, которые хотите выполнить \n" +
+                "например: 1 + 2; 2 * 3; 3 / 4; 4 - 5 \n" +
+                "Если захотите выйдти из приложенияя просто введите ENTER");
             while (true)
             {
                 string? Line = Console.ReadLine();
-                if ((Line == null)||(Line == "")) { break; }
+                if ((Line == null) || (Line == ""))
+                {
+                    break;
+                }
                 string[] Operands = Line.Split(' ');
-                double? result = Calculation(Operands);
-                if (result != null) { Console.WriteLine("Результат "+result); }
+                try
+                {
+                    double? result = Calculation(Operands);
+                    Console.WriteLine("Результат " + result);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
             }
             Console.WriteLine("Программа закочила свою работу");
             return 0;
         }
 
-        private static double? Calculation(string[] Operands) 
+        private static double? Calculation(string[] Operands)
         {
-            if(Operands.Length != 3) 
+            if (Operands.Length != 3)
             {
-                Console.Error.WriteLine("<Первое_число> <Математическая_операция> <Второе_число>");
-                return null; 
+                throw new Exception("<Первое_число> <Математическая_операция> <Второе_число>");
             }
-            double? firstNumber = StringToDouble(Operands[0]);
-            if (firstNumber == null) { return null; };
-            double? secondNumber = StringToDouble(Operands[2]);
-            if(secondNumber == null) { return null; };
-            switch (Operands[1]) 
-            { 
+            double firstNumber = ValidateValue(Operands[0]);
+            double secondNumber = ValidateValue(Operands[2]);
+            switch (Operands[1])
+            {
                 case "+":
                     return firstNumber + secondNumber;
                 case "-":
@@ -38,33 +48,26 @@
                 case "*":
                     return firstNumber * secondNumber;
                 case "/":
-                    if(secondNumber == 0) 
+                    if (secondNumber == 0)
                     {
-                        Console.WriteLine("Клёво на {0} делишь",secondNumber);
-                        return null; 
+                        throw new Exception("Ноль не может быть делителем");
                     };
                     return firstNumber / secondNumber;
                 default:
-                    Console.WriteLine("не предусмотренная математическая операция(+,-,*,/)");
-                    return null;
-            } 
-        }
-        private static Double? StringToDouble(string maybeInt) 
-        {
-            if (maybeInt != null) { 
-                Double output = 0;
-                try { 
-                    output = Convert.ToDouble(maybeInt);
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("{0} не число",maybeInt); 
-                    return null; 
-                }
-                catch (OverflowException) {Console.WriteLine("Число {0} слишком большое",maybeInt); return null; };
-                return output; 
+                    throw new Exception("не предусмотренная математическая операция(+,-,*,/)");
             }
-            return null; 
+        }
+        private static double ValidateValue(string value)
+        {
+            if (!double.TryParse(value, out double number))
+            {
+                throw new Exception($"Значение - {value}, нельзя привести к типу double");
+            }
+            if (number == double.PositiveInfinity || number == double.NegativeInfinity)
+            {
+                throw new Exception($"Значение {value} слишком бльшое");
+            }
+            return number;
         }
     }
 
